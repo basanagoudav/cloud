f1d8b32ccd944fd5c810718b39041334d9654242
Basanagouda <basanagouda.v@saviynt.com>
2019-08-05 12:48:06 +0530

    my first sprring cloud project

---
+++ employee-producer/src/main/java/com/eureka/client/GreetingController.java
@@ -0,0 +1,23 @@
+package com.eureka.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.discovery.EurekaClient;
+
+@RestController
+@RequestMapping("/employeeGreetings")
+public class GreetingController {
+    @Value("${server.port}")
+    private String port;
+	
+    @GetMapping
+	public String greeting() {
+        return "Hello you are welcome, i am serving you at port : "+port;
+    }
+
+}
---
+++ empoyee-consumer/src/main/java/com/employee/consumer/EmpoyeeConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.employee.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpoyeeConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpoyeeConsumerApplication.class, args);
+	}
+
+}
