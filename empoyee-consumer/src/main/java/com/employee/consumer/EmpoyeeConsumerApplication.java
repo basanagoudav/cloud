2de25a5cbd4c712d2430e3f4c7637dc3f977b782
Anand B <anand.basavaraju@kdi.kongsberg.com>
2019-12-29 10:24:56 +0530

    java file

--- empoyee-consumer/src/main/java/com/employee/consumer/EmpoyeeConsumerApplication.java
+++ empoyee-consumer/src/main/java/com/employee/consumer/EmpoyeeConsumerApplication.java
@@ -8,6 +8,10 @@ public class EmpoyeeConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmpoyeeConsumerApplication.class, args);
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
 	}
 
 }
